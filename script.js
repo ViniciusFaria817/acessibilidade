bf72d99319f6ab95e50e777429435a64f9b76c12
ViniciusFaria817 <00001115433805sp@al.educacao.sp.gov.br>
2025-08-25 07:40:48 -0300

    v3

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-document.addEventListener('DOMContentLoaded', funtion()){
-   
-    
+document.addEventListener('DOMContentLoaded', function () {
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
     let tamanhoAtualFonte = 1;
-    aumentarFonteBotao.addEvent('click', function(){
-        tamanhoAtual+= 0.1;
-        document.body.style.fontSize = '${tamanhoAtualFonte}rem`'
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
-}//falta parentese?
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+})
